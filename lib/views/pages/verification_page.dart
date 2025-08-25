50c26e6d2ee4c90027f6dfe39e868e090bdddbb8
SalahBadreddine <salah.badreddine@ensia.edu.dz>
2025-08-25 15:46:06 +0100

    added verification page

--- lib/views/pages/verification_page.dart
+++ lib/views/pages/verification_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/constants/classes.dart';
 import 'package:food_delivery/views/widgets/orange_button_widget.dart';
 import 'package:food_delivery/views/widgets/page_title_widget.dart';
-import 'package:food_delivery/views/widgets/textfield_widget.dart';
 
 class VerificationPage extends StatefulWidget {
   const VerificationPage({super.key});
@@ -35,25 +35,25 @@ class _VerificationPageState extends State<VerificationPage> {
         children: [
           Flexible(
             flex: 2,
-              child: Center(
-                child: Column(
-                  children: [
-                    PageTitleWidget(
-                      title: "Verification",
-                      subtitle: "We have sent a code to your email",
+            child: Center(
+              child: Column(
+                children: [
+                  PageTitleWidget(
+                    title: "Verification",
+                    subtitle: "We have sent a code to your email",
+                  ),
+                  Text(
+                    "example@gmail.com",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
                     ),
-                    Text(
-                      "example@gmail.com",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
+          ),
           Expanded(
             flex: 7,
             child: Container(
@@ -66,12 +66,61 @@ class _VerificationPageState extends State<VerificationPage> {
                 ),
               ),
               child: Padding(
-                padding: EdgeInsetsGeometry.all(20),
+                padding: EdgeInsetsGeometry.all(30),
                 child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    TextfieldWidget(
-                      title: "CODE",
-                      placeholder: "example@gmail.com",
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("CODE", style: KTextStyle.TextfieldInfo),
+                        RichText(
+                          text: TextSpan(
+                            text: "Resend",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              decoration: TextDecoration.underline,
+                            ),
+                            children: [
+                              TextSpan(
+                                text: " in.50sec",
+                                style: TextStyle(color: Color(0xFF32343E), fontWeight: FontWeight.normal, decoration: TextDecoration.none,),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 8),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: List.generate(4, (index) {
+                        return SizedBox(
+                          width: 62,
+                          height: 62,
+                          child: TextFormField(
+                            textAlign: TextAlign.center,
+                            style: const TextStyle(
+                              color: Color(0xFF32343E),
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            keyboardType: TextInputType.number,
+                            maxLength: 1, // only 1 digit per box
+                            decoration: InputDecoration(
+                              counterText: "", // hide bottom counter
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                                borderSide: BorderSide.none,
+                              ),
+                              filled: true,
+                              fillColor: const Color(0xFFF0F5FA),
+                              contentPadding: const EdgeInsets.all(20),
+                            ),
+                          ),
+                        );
+                      }),
                     ),
                     Padding(
                       padding: const EdgeInsets.symmetric(vertical: 20.0),
