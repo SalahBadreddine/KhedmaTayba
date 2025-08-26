3666aaad6b63b79cb7188fb94f166c81c4a989d4
SalahBadreddine <salah.badreddine@ensia.edu.dz>
2025-08-27 00:16:20 +0100

    started with search page

--- lib/views/pages/search_page.dart
+++ lib/views/pages/search_page.dart
@@ -16,56 +16,83 @@ class _SearchPageState extends State<SearchPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.all(30),
-          child: NavigationWidget(
-            onTapLeadingButton: () => Navigator.pop(context),
-            icon1: Icons.arrow_back_ios,
-            title: Text(
-              "Search",
-              style: TextStyle(fontSize: 17, color: AppColors.homeDark),
-            ),
-            suffixicon: Stack(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                SizedBox(height: 49, width: 45),
-                Positioned(
-                  bottom: 0,
-                  child: Container(
-                    width: 45,
-                    height: 45,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: AppColors.homeDark,
-                    ),
-                    child: IconButton(
-                      onPressed: () {},
-                      icon: Icon(Icons.shopping_cart),
-                      color: Colors.white,
-                    ),
+                NavigationWidget(
+                  onTapLeadingButton: () => Navigator.pop(context),
+                  icon1: Icons.arrow_back_ios,
+                  title: Text(
+                    "Search",
+                    style: TextStyle(fontSize: 17, color: AppColors.homeDark),
                   ),
-                ),
-                Positioned(
-                  top: 0,
-                  right: 0,
-                  child: Container(
-                    height: 25,
-                    width: 25,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: AppColors.primaryColor,
-                    ),
-                    child: Center(
-                      child: Text(
-                        numcartitems,
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 16,
-                          fontWeight: FontWeight.w900,
+                  suffixicon: Stack(
+                    children: [
+                      SizedBox(height: 49, width: 45),
+                      Positioned(
+                        bottom: 0,
+                        child: Container(
+                          width: 45,
+                          height: 45,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: AppColors.homeDark,
+                          ),
+                          child: IconButton(
+                            onPressed: () {},
+                            icon: Icon(Icons.shopping_cart),
+                            color: Colors.white,
+                          ),
                         ),
                       ),
-                    ),
+                      Positioned(
+                        top: 0,
+                        right: 0,
+                        child: Container(
+                          height: 25,
+                          width: 25,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: AppColors.primaryColor,
+                          ),
+                          child: Center(
+                            child: Text(
+                              numcartitems,
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w900,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
+                SizedBox(height: 30),
+                TextFormField(
+                  autofocus: true,
+                  decoration: InputDecoration(
+                    hintText: "Search dishes, restaurants",
+                    filled: true,
+                    fillColor: Color(0xFFF6F6F6),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide.none,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    prefixIcon: Icon(
+                      Icons.search,
+                      color: AppColors.iconColor,
+                    ),
+                    contentPadding: EdgeInsets.all(20),
+                  ),
+                ),
+                SizedBox(height: 30),
+                
               ],
             ),
           ),
